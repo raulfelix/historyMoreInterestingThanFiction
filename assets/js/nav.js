2f5bc3a9ee23cac46cf8e3a4736d8d32518b6db6
Raul Felix Carrizo <raulfelixcarrizo@gmai.com>
2016-01-17 21:42:16 +1100

    Layout and style updates prior to first deployment

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,47 +1,25 @@
 (function() {
-  
+
   //
   // Navigation controller
   //
   var Navigation = (function() {
-    
-    var
-      body,
-      trigger,
-      modalTrigger;
-    
-    
-    function open() {
-      body.classList.add('active');  
-    }
-    
-    function close() {
-      body.classList.remove('active'); 
-    }
-    
+
     function up() {
-      Velocity(body, "scroll", { offset: "0px"});
+      Velocity(document.body, 'scroll', { offset: '0px'});
     }
-    
+
     function init() {
-      body = document.querySelector('body');
-      trigger = document.querySelector('#nav-open');
-      modalTrigger = document.querySelector('#nav-close');
-      
-      var backToTop = document.querySelector('#to-top');
-      
-      // todo browser support
-      trigger.addEventListener('click', open, false);
-      modalTrigger.addEventListener('click', close, false);
-      backToTop.addEventListener('click', up, false);
+      // var backToTop = document.querySelector('#to-top');
+      // backToTop.addEventListener('click', up);
     }
-    
+
     return {
       init: init
     };
-    
+
   })();
-  
+
   Navigation.init();
-  
+
 })();
